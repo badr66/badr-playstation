25601e93b3790447d7e0bedbb0611efa90eca605
baselbalkes <baselbalkes77@gmail.com>
2024-10-24 10:22:33 +0300

    fix(fetch user data)

--- src/components/MainContainer/Sidebar.tsx
+++ src/components/MainContainer/Sidebar.tsx
@@ -6,16 +6,24 @@ import Logout from "../logout/Logout";
 import EditUser from "../editUser/EditUser";
 import { useUser } from "@/context/userContext";
 import { getUserInfo } from "@/databaseFunctions/users/getUserInfo";
+import { useEffect } from "react";
 
-export default async function Sidebar() {
+export default function Sidebar() {
     const {user} = useUser();
-    let password;
-    if(user) {
-        const callApi = await getUserInfo(user.name);
-        if(callApi.password) {
-            password = callApi.password;
-        }
-    }
+    let password = "";
+    useEffect(() => {
+        const fetchUserInfo = async () => {  
+            if (user) {  
+                const callApi = await getUserInfo(user.name);  
+                if (callApi.password) {  
+                    password = callApi.password;  
+                }  
+            }  
+        };  
+    
+        fetchUserInfo(); 
+    },[user])
+
     return(
         <div className={styles.sidebar}>
             <div className={styles.sidebarHeader}>
